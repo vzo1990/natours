7d15ad9a41d3534c794d7bb48c1ee7f047d224d4
Vitalii Zorichev <zorichevitaliy@gmail.com>
2023-09-05 14:23:55 -0500

    Fix for booking creation

--- controllers/bookingController.js
+++ controllers/bookingController.js
@@ -40,8 +40,8 @@ exports.getCheckoutSession = catchAsync(async (req, res, next) => {
 
 const createBookingFromSession = catchAsync(async (session) => {
   const tour = session.client_reference_id;
-  const user = await User.findOne({ email: session.customer_email }).id;
-  const price = session.line_items[0].price_data.unit_amount / 100;
+  const user = (await User.findOne({ email: session.customer_email })).id;
+  const price = session.amount_total / 100;
 
   await Booking.create({
     tour,
